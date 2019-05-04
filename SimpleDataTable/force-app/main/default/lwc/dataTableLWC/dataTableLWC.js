82337d3bbcd13ed6375693073960a85d0edc2411
Julfy Patlatus <julfy@i.ua>
2019-05-04 18:29:13 +0300

    Simple Data Table App: Follow DRY principle and clean up duplicated code and variables https://salesforce.stackexchange.com/questions/260779/unable-to-reuse-code-from-component-a-in-component-b/261143

--- SimpleDataTable/force-app/main/default/lwc/dataTableLWC/dataTableLWC.js
+++ SimpleDataTable/force-app/main/default/lwc/dataTableLWC/dataTableLWC.js
@@ -1,17 +1,18 @@
-import { LightningElement, track, api } from 'lwc';
+import { api } from 'lwc';
 import getColumnsAndData from '@salesforce/apex/DataTableController.getColumnsAndData';
-export default class DataTableLWC extends LightningElement {
-    @api sObjectName;
-    @api sObjectFieldsNames;
-    @api whereClause;
+import SimpleDataTableLWC from 'c/simpleDataTableLWC';
+export default class DataTableLWC extends SimpleDataTableLWC {
+    //@api sObjectName;
+    //@api sObjectFieldsNames;
+    //@api whereClause;
     @api overrides;
     @api valueModifiers;
 
-    @track data;
-    @track columns;
-    @track error;
-    @track sortedBy;
-    @track sortDirection;
+    //@track data;
+    //@track columns;
+    //@track error;
+    //@track sortedBy;
+    //@track sortDirection;
     connectedCallback() {
         getColumnsAndData({ sObjectName:this.sObjectName, sObjectFieldsNames:this.sObjectFieldsNames.split(',')
             , whereClause: this.whereClause, overrides: JSON.parse(this.overrides.replace(/'/g, '"'))
@@ -22,34 +23,5 @@ export default class DataTableLWC extends LightningElement {
         }).catch(error => {
             this.error = error;
         });
-    }
-    
-    // Client-side controller called by the onsort event handler
-    updateColumnSorting(e) {
-        this.sortedBy = e.detail.fieldName;
-        this.sortDirection = e.detail.sortDirection;
-        //e.srcElement.sortedBy = e.detail.fieldName;
-        //e.srcElement.sortDirection = e.detail.sortDirection;
-        this.sortData(e.detail.fieldName, e.detail.sortDirection);
-    }
-
-    sortData(fieldName, sortDirection) {
-        var reverse = sortDirection !== 'asc';
-        //sorts the rows based on the column header that's clicked
-        var data = this.data;
-        data.sort(this.sortBy(fieldName, reverse));
-        this.data = JSON.parse(JSON.stringify(data)) ;
-    }
-    sortBy(field, reverse, primer) {
-        var key = primer ?
-            function(x) {return primer(x[field])} :
-            function(x) {return x[field]};
-        //checks if the two rows should switch places
-        reverse = !reverse ? 1 : -1;
-        return function (a, b) {
-            a = key(a);
-            b = key(b);
-            return reverse * ((a > b) - (b > a));
-        }
     }
 }
